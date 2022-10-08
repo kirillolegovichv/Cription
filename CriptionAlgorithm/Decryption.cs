f0b7048b46945e906d373565d7107b2381436c1d
Kirill <kirill.vasil.oleg@gmail.com>
2022-10-08 16:31:30 +0300

    Add decryption method

---
+++ CriptionAlgorithm/Decryption.cs
@@ -0,0 +1,24 @@
+﻿namespace CriptionAlgorithm;
+
+public class Decryption
+{
+    string alphabet = "АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЫЬЭЮЯ";
+    string encryptAlphabet = "ЦУКЕНГШЩЗХФЫВАПРОЛДЖЭЯЧСМИТЬБЮ";
+
+    public string DecryptSequence(string sequence)
+    {
+        string encryptedSequence = "";
+
+        for (int i = 0; i < sequence.Length; i++)
+        {
+            for (int j = 0; j < encryptAlphabet.Length; j++)
+            {
+                if (sequence[i] == encryptAlphabet[j])
+                {
+                    encryptedSequence += alphabet[j];
+                }
+            }
+        }
+        return encryptedSequence;
+    }
+}
--- CriptionAlgorithm/Program.cs
+++ CriptionAlgorithm/Program.cs
@@ -1,7 +1,10 @@
 ﻿using CriptionAlgorithm;
 
 Encryption enc = new Encryption();
+Decryption dec = new Decryption();
 
 string seq = Console.ReadLine();
 
 Console.WriteLine(enc.EncryptSequence(seq));
+string text = Console.ReadLine();
+Console.WriteLine(dec.DecryptSequence(text));
