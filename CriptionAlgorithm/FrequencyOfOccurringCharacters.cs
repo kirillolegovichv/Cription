2c657d9aef96f34c399726ea858901db74ac87fd
Kirill <kirill.vasil.oleg@gmail.com>
2022-10-16 15:23:50 +0300

    Add method counting frequency

--- CriptionAlgorithm/FrequencyOfOccurringCharacters.cs
+++ CriptionAlgorithm/FrequencyOfOccurringCharacters.cs
@@ -2,4 +2,19 @@
 
 public class FrequencyOfOccurringCharacters
 {
+    public void CountingFrequencyOfOccurringCharacters(string sequence)
+    {
+        Dictionary<string, int> characters = new Dictionary<string, int>();
+        for(int i = 0; i < sequence.Length; i++)
+        {
+            if (!characters.ContainsKey(sequence[i].ToString()))
+            {
+                characters.Add(sequence[i].ToString(), 1);
+            }
+            else
+            {
+                characters[sequence[i].ToString()]++;
+            }
+        }
+    }
 }
